612171842d691af539aff3f43eb837fa6b03f2b7
cffireblackvv1991 <crisfer2791@gmail.com>
2025-06-21 13:25:35 -0500

    second commit

---
+++ prueba.js
@@ -0,0 +1,14 @@
+
+
+
+console.log("repasando github");
+
+let numeroUno = 10;
+let numeroDos = 20;
+
+
+if (numeroUno > numeroDos) {
+    console.log("condicion verdadera");
+}else {
+    console.log("condicion falsa");
+}
